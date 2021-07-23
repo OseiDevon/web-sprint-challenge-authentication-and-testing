ab9275bf5bc41f66aeba00394b76257a1d3697f8
Devon Osei <oseidevon@gmail.com>
2021-07-23 15:33:15 -0400

    hey

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,4 +1,9 @@
-const router = require('express').Router();
+const router = require('express').Router()
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const Users = require("./users-model")
+const { restrict } = require("./users-middleware")
+
 
 router.post('/register', (req, res) => {
   res.end('implement register, please!');
@@ -29,6 +34,49 @@ router.post('/register', (req, res) => {
   */
 });
 
+
+router.get("/users", restrict(), async (req, res, next) => {
+  try {
+      res.json(await Users.find())
+  } catch(err) {
+      next(err)
+  }
+})
+
+router.post("/login", async (req, res, next)=>{
+  try {
+      const { username, password } = req.body
+  const user = await Users.findByUsername(username)
+
+  if (!user) {
+    return res.status(401).json({
+      message: "Invalid Credentials",
+    })
+      }
+      const passwordValid = await bcrypt.compare(password, user.password)
+
+  if (!passwordValid) {
+    return res.status(401).json({
+      message: "Invalid Credentials",
+    })
+      }
+      const token = jwt.sign({
+    userID: user.id,
+    userDepartment: user.department,
+
+      }, process.env.JWT_SECRET)
+
+      res.cookie("token", token)
+
+  res.json({
+    message: `Welcome ${user.username}!`,
+  })
+
+  } catch(err) {
+      next(err)
+  }
+});
+
 router.post('/login', (req, res) => {
   res.end('implement login, please!');
   /*
@@ -55,5 +103,42 @@ router.post('/login', (req, res) => {
       the response body should include a string exactly as follows: "invalid credentials".
   */
 });
+
+router.post("/register", async (req, res, next)=>{
+  try {
+      const { username, password, department } = req.body
+      const user = await Users.findByUsername(username)
+
+      if (user) {
+    return res.status(409).json({
+      message: "Username is already taken",
+    })
+      }
+
+      const newUser = await Users.add({
+          username,
+          department,
+          password: await bcrypt.hash(password, 14)
+      })
+
+      res.status(201).json(newUser)
+  } catch(err) {
+      next(err)
+  }
+});
+
+router.get("/logout", async (req, res, next) => {
+	try {
+		req.session.destroy((err) => {
+			if (err) {
+				next(err)
+			} else {
+				res.status(204).end()
+			}
+		})
+	} catch (err) {
+		next(err)
+	}
+})
 
 module.exports = router;
--- api/server.test.js
+++ api/server.test.js
@@ -1,3 +1,43 @@
+const supertest = require("")
+const server = require("../server")
+
+test("GET /", async () => {
+    const res = await supertest(server).get("/")
+    expect(res.statusCode).toBe(200)
+    expect(res.type).toBe("application/json")
+    expect(res.body.message).toBe("Welcome to our API")
+})
+
+const supertest = require("supertest")
+const server = require("../server")
+const db = require("../data/config")
+
+beforeEach(async () => {
+    await db.seed.run()
+})
+
+afterAll(async () => {
+    await db.destroy()
+})
+
+describe("jokes integration tests", () => {
+    it("gets a list of jokes", async () => {
+        const res = await supertest(server).get("/jokes")
+        expect(res.statusCode).toBe(200)
+        expect(res.type).toBe("application/json")
+        expect(res.body.length).toBe(4)
+    })
+})
+
+
+it("get the joke by ID", async () => {
+    const res = await supertest(server)
+    .post("/jokes")
+    .send({ name: "bilbo"})
+    expect(res.statusCode).toBe(201)
+    expect(res.type).toBe("application?json")
+})
+
 // Write your tests here
 test('sanity', () => {
   expect(true).toBe(false)
